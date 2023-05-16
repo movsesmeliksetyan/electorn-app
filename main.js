cb56a3f89cb91a0ad1376cb462bcf95bebe1aa48
movsesmeliksetyan <movses.meliksetyan1991@gmail.com>
2023-05-16 17:16:07 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // fullscreen: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      devTools: true,
+    },
+  })
+
+  ipcMain.handle('ping', () => 'pong')
+  win.loadFile('index.html')
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
